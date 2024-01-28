a6e4298c04dab2e9875a9a26a1aeb1eeca337a3b
Swastik Raj Vansh SIngh <95278835+swastik-raj-vansh-singh@users.noreply.github.com>
2024-01-28 13:40:26 +0530

    adding account page

--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -5,12 +5,12 @@ import { useRouter } from "next/navigation";
 
 import { useUser } from "@/hooks/useUser";
 import Button from "@/components/Button";
-import useSubscribeModal from "@/hooks/useSubscribeModal";
-import { postData } from "@/libs/helpers";
+// import useSubscribeModal from "@/hooks/useSubscribeModal";
+// import { postData } from "@/libs/helpers";
 
 const AccountContent = () => {
   const router = useRouter();
-  const subscribeModal = useSubscribeModal();
+  // const subscribeModal = useSubscribeModal();
   const { isLoading, subscription, user } = useUser();
 
   const [loading, setLoading] = useState(false);
@@ -21,18 +21,18 @@ const AccountContent = () => {
     }
   }, [isLoading, user, router]);
 
-  const redirectToCustomerPortal = async () => {
-    setLoading(true);
-    try {
-      const { url, error } = await postData({
-        url: '/api/create-portal-link'
-      });
-      window.location.assign(url);
-    } catch (error) {
-      if (error) return alert((error as Error).message);
-    }
-    setLoading(false);
-  };
+  // const redirectToCustomerPortal = async () => {
+  //   setLoading(true);
+  //   try {
+  //     const { url, error } = await postData({
+  //       url: '/api/create-portal-link'
+  //     });
+  //     window.location.assign(url);
+  //   } catch (error) {
+  //     if (error) return alert((error as Error).message);
+  //   }
+  //   setLoading(false);
+  // };
 
   return ( 
     <div className="mb-7 px-6">
@@ -40,7 +40,7 @@ const AccountContent = () => {
         <div className="flex flex-col gap-y-4">
         <p>No active plan.</p>
         <Button 
-          onClick={subscribeModal.onOpen}
+          onClick={()=>{}}
           className="w-[300px]"
         >
           Subscribe
@@ -55,7 +55,7 @@ const AccountContent = () => {
           </p>
           <Button
             disabled={loading || isLoading}
-            onClick={redirectToCustomerPortal}
+            onClick={()=>{}}
             className="w-[300px]"
           >
             Open customer portal
